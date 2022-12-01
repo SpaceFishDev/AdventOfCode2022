6c180bad1380c3c03df42f3e6b45e9ad26002f3b
SpaceFishGameDev <ultragamesalot@gmail.com>
2022-12-01 17:17:40 +0200

    Read input from file

--- Day 1/src/main.cpp
+++ Day 1/src/main.cpp
@@ -1,13 +1,18 @@
 #include<string>
 #include<iostream>
 #include<vector>
+#include <bits/stdc++.h>
+#include<fstream>
 
-std::vector<std::string> SplitBySpace(std::string str){
+
+
+
+std::vector<std::string> SplitByNLine(std::string str){
 	const char* string = &str[0];
 	std::string temp = "";
 	std::vector<std::string> result;
 	while(*string){
-		if(*string == ' '){
+		if(*string == '\n'){
 			result.push_back(temp);
 			temp = "";
 		}
@@ -28,63 +33,33 @@ int GetTotal(std::vector<std::string> strs){
 	return num;
 }
 
-int partition(int integers[], int start, int end){
-	int pivot = integers[start];
-	int count = 0;
-	for(int i = start + 1; i <= end; ++i){
-		if(integers[i] <= pivot){
-			++count;
-		}
-	}
-
-	int pivot_index = start + count;
-	std::swap(integers[pivot_index], integers[start]);
-
-	int i = start, j = end;
-
-	while(i < pivot_index && j < pivot_index){
-		while(integers[i] <= pivot){
-			++i;
-		}
-		while(integers[j] > pivot){
-			--j;
-		}
-		if(i < pivot_index && j > pivot_index){
-			std::swap(integers[i++], integers[j--]);
-		}
-	}
-	return pivot_index;
-}
-
-void quickSort(int integers[], int start, int end){
-	if(start >= end){
-		return;
-	}
-	int p = partition(integers, start, end);
-	quickSort(integers, start, p - 1);
-	quickSort(integers, p + 1, end);
-}
-
 
 int main(){
 	std::vector<std::vector<std::string>> lines; 
-	while(true){
-		std::string line;
-		std::getline(std::cin, line);
+	std::string temp = "";
+	std::ifstream infile("input.txt");
+	std::string line;
+	while(	std::getline(infile, line)){
 		if(line == "exit"){
 			break;
 		}
-		if(line != "" && line != "\t" && line != " "){
-			lines.push_back(SplitBySpace(line));
+		if(line == "" || line == "\t" || line == " "){
+			lines.push_back(SplitByNLine(temp));
+			temp = "";
+		}else
+		{
+			temp += line + "\n";
 		}
 	}
 	std::vector<int> totals;
 	for(std::vector<std::string> line : lines){
 		totals.push_back(GetTotal(line));
-		printf("\n");
 	}
-	int* arr = &totals[0];
-	quickSort(arr, 0, totals.size() - 1);
-	std::cout << totals[totals.size() - 1];
+	std::sort(totals.begin(), totals.end());
+	for(int total : totals){
+		std::cout << total << "\n";
+	}
+	std::cout<< "Largest = " << totals[totals.size() - 1] << "\n";
+
 	return 0;
 }
